d071730b002ce867b8670062ae97d03150b22ea4
shojiro koga <s_koga@nttpc.co.jp>
2018-02-27 04:38:57 +0000

    init

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -19,5 +19,5 @@ WickedPdf.config = {
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
   
-   :exe_path => '/usr/local/rvm/gems/ruby-2.4.0/bin/wkhtmltopdf'
+   exe_path: "#{Gem.loaded_specs['wkhtmltopdf-binary'].full_gem_path}/bin/wkhtmltopdf"
 }
