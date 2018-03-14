eebe8c6fdca448476d56a1530ab00e24de972bed
shojiro koga <s_koga@nttpc.co.jp>
2018-03-14 03:05:34 +0000

    test commit

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 require 'csv'
 require 'roo'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 
 
-module Kadai2
+module Asset_Manage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -21,5 +23,9 @@ module Kadai2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
   end
 end
